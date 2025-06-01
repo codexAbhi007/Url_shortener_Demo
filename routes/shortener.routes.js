fa038d9c83d258cc0e5094185a7fde51b7630542
codexAbhi007 <codewithabhi2024@gmail.com>
2025-06-01 19:49:37 +0530

    add resuable components

--- routes/shortener.routes.js
+++ routes/shortener.routes.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from "url";
 const router = Router()
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename); 
 const DATA_FILE = path.join(__dirname,"..", "urlData.json");
 // console.log(__filename)
 
